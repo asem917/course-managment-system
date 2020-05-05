3bf68d348b214c1bf04c59b8897ee811d26529ac
Magic Systems <asem@gmail.com>
2020-05-05 16:41:42 +0300

    first

---
+++ src/controller/dispach/ServiceLocator.java
@@ -0,0 +1,29 @@
+package controller.dispach;
+
+
+import controller.controllers.Controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ServiceLocator {
+
+    private static final Map<String, Controller> controllers;
+
+    static {
+        controllers = new HashMap<String, Controller>();
+        controllers.put("login", );
+        controllers.put("signup",);
+        controllers.put("list",);
+        controllers.put("register");
+    }
+
+    private static AtomicInteger count = new AtomicInteger(0);
+    public static Controller getController (String controllerName) {
+        synchronized (ServiceLocator.class) {
+            count.getAndIncrement();
+        }
+        return controllers.get(controllerName);
+    }
+}
